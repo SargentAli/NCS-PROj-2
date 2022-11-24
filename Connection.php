9ad72723da7b733a51910e55c2f706e548e22fdc
SargentAli <2000356b@student.tp.edu.sg>
2022-11-24 07:03:19 +0000

    hii

--- Connection.php
+++ Connection.php
@@ -2,7 +2,7 @@
 	$Host = "glennserv.mysql.database.azure.com";
 	$Username = "glenn";
 	$Password = "P@ssw0rd";
-	$Db = "shoe-db";
+	$Db = "smss_db";
 	
 	$Conn = mysqli_connect($Host,$Username,$Password,$Db);
 
