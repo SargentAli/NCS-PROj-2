e86e40b602050619c6486dfd3fbbfb0e67dd7cd1
SargentAli <95275905+SargentAli@users.noreply.github.com>
2023-02-01 15:36:31 +0800

    Update Connection.php

--- Connection.php
+++ Connection.php
@@ -1,7 +1,7 @@
 <?php
-	$Host = "glennserv.mysql.database.azure.com";
-	$Username = "glenn";
-	$Password = "P@ssw0rd";
+	$Host = "database-2.cdlocju08nwb.us-east-1.rds.amazonaws.com";
+	$Username = "admin";
+	$Password = "password";
 	$Db = "smss_db";
 	
 	$Conn = mysqli_connect($Host,$Username,$Password,$Db);
